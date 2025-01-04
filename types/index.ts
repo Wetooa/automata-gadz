999724f7971e26422027185cc8e62094982ab7c1
wetooa <derpykidyt@gmail.com>
2025-01-05 02:20:49 +0800

    removing bad valid regex implementation

--- types/index.ts
+++ types/index.ts
@@ -1,4 +1,15 @@
-export interface ValidationResultProps {
+export interface ValidationResult {
   isValid: boolean;
   error: string[];
+}
+
+type State = `q${string}`;
+type Alphabet = `${string}`;
+
+export interface TransitionTable<S extends State, A extends Alphabet> {
+  startState: S;
+  endStates: S[];
+  transitionTable: {
+    [K in S]: Record<A, S>;
+  };
 }
--- util/regex.ts
+++ util/regex.ts
@@ -1,4 +1,4 @@
-import { ValidationResultProps } from "@/types";
+import { TransitionTable, ValidationResult } from "@/types";
 
 function isBalancedParenthesis(string: string) {
   let parenthesisCount = 0;
@@ -13,10 +13,11 @@ function isBalancedParenthesis(string: string) {
   return parenthesisCount === 0;
 }
 
-export function isValidRegex(regex: string): ValidationResultProps {
+// NOTE: Ignore for now
+function isValidRegexWrong(regex: string): ValidationResult {
   // FIX: THIS IS VERY WRONG, LETS USE DFA METHOD INSTEAD, WILL BE CREATING A DIAGRAM
 
-  const result: ValidationResultProps = { isValid: true, error: [] };
+  const result: ValidationResult = { isValid: true, error: [] };
 
   if (!isBalancedParenthesis(regex)) {
     result.isValid = false;
@@ -24,29 +25,48 @@ export function isValidRegex(regex: string): ValidationResultProps {
     return result;
   }
 
-  // const ignoredCharacters = new RegExp("[a-z]\(\)\ ");
-  //
-  // for (let i = 0; i < regex.length; i++) {
-  //   if (regex[i] === "*") {
-  //     if (i === 0 || regex[i - 1] === "*" || regex[i - 2] === "+") {
-  //       result.isValid = false;
-  //       result.error.push("Invalid '*' was found!");
-  //       break;
-  //     }
-  //
-  //   } else if (regex[i] === '+') {
-  //     if (i === 0 || regex[i - 1] === '')
-  //   } else if (ignoredCharacters.test(regex[i])) {
-  //     // ignore the character
-  //   } else {
-  //     // NOTE: Possibly useless catch (input will not accept invalid characters)
-  //
-  //     result.isValid = false;
-  //     result.error.push("An invalid character was found!");
-  //     break;
-  //   }
-  //   i++;
-  // }
+  const ignoredCharacters = new RegExp("[a-z]\(\)\ ");
+
+  for (let i = 0; i < regex.length; i++) {
+    if (regex[i] === "*") {
+      if (i === 0 || regex[i - 1] === "*" || regex[i - 2] === "+") {
+        result.isValid = false;
+        result.error.push("Invalid '*' was found!");
+        break;
+      }
+
+    } else if (regex[i] === '+') {
+      if (i === 0 || regex[i - 1] === '')
+    } else if (ignoredCharacters.test(regex[i])) {
+      // ignore the character
+    } else {
+      // NOTE: Possibly useless catch (input will not accept invalid characters)
+
+      result.isValid = false;
+      result.error.push("An invalid character was found!");
+      break;
+    }
+    i++;
+  }
 
   return result;
+}
+
+type validRegexStates = "q0" | "q1" | "q2"
+
+export const validRegexTransitionTable : TransitionTable = {
+endStates: ["q1", "q2"],
+  transitionTable: {
+  }
+}
+
+export function isValidRegex(regex: string): ValidationResult {
+  const result: ValidationResult = { isValid: true, error: [] };
+
+  if (!isBalancedParenthesis(regex)) {
+    result.isValid = false;
+    result.error.push("Expression is not a balanced paranthesis!");
+    return result;
+  }
+
 }
