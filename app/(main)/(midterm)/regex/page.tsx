bc4c0f5c339a2e8d3da63f64195b7e17a6208e1b
Frenz R <125328585+ChimaRyder@users.noreply.github.com>
2025-01-09 01:37:47 +0800

    modified the page to display the result of the object on the frontend.
    Only took "isValid" from the object and displayed it below, nothing really special. Used a couple of hooks to identify where each one is.
    
    Idk if you want this Adrian, you might already be working on a frontend functionality already lmao

--- app/(main)/(midterm)/regex/page.tsx
+++ app/(main)/(midterm)/regex/page.tsx
@@ -1,16 +1,28 @@
+'use client'
+
 import { Input } from "@/components/ui/input";
 import { isValidRegex } from "@/util/regex";
-import React from "react";
+import React, {useState} from "react";
 
 export default function Regex() {
+    const [valid, isValid] = useState(false);
+    const [regex, setRegex] = useState("");
+
   console.log(isValidRegex("ab**c"));
   console.log(isValidRegex("a(a+b)"));
   console.log(isValidRegex("b+(a*b*)"));
 
+  const handleRegex = (e) => {
+      if (e.key == "Enter") {
+          isValid(isValidRegex(regex).isValid)
+      }
+  }
+
   return (
     <div className="w-full h-full p-10 ">
       <h1 className="text-3xl">Regex</h1>
-      <Input />
+      <Input onKeyUp={handleRegex} onChange={(e) => setRegex(e.target.value)}/>
+        <h2>Result: {valid.toString()}</h2>
     </div>
   );
 }
